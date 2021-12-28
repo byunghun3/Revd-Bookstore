5a78ec9a6022dccbb1766471215e53a759cd746f
Byung-Hun Kim <byunghun3@gmail.com>
2021-12-28 12:22:03 -0500

    add error page route

--- src/components/PageRoutes/PageRoutes.js
+++ src/components/PageRoutes/PageRoutes.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import About from '../../pages/About/About';
 import Browse from '../../pages/Browse/Browse';
 import Contact from '../../pages/Contact/Contact';
+import Error from '../../pages/Error/Error';
 
 function PageRoutes() {
     return (
@@ -11,6 +12,7 @@ function PageRoutes() {
             <Route path='/about' element={<About/>} />
             <Route path='/browse' element={<Browse/>} /> 
             <Route path='/contact' element={<Contact/>} />
+            <Route path='*' element={<Error/>} />
         </Routes>
     )
 }
---
+++ src/pages/Error/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Error.css';
+
+function Error() {
+    return (
+        <div className="error">
+            Page Not Found
+        </div>
+    )
+}
+
+export default Error;
