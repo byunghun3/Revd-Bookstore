f43d677dd7f1a0ff5f5b82da182dc2a341340c7b
Byung-Hun Kim <byunghun3@gmail.com>
2021-12-27 23:36:46 -0500

    import google fonts

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import About from './components/About/About';
 import Browse from './components/Browse/Browse';
 import Contact from './components/Contact/Contact';
 import { Route, Routes, NavLink } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <NavLink to='/about'>About</NavLink>
       <NavLink to='/browse'>Browse</NavLink>
       <NavLink to='/contact'>Contact</NavLink>
