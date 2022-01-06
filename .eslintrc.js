76798ff6f35b625c795606f2796bbb61d7bc1ac5
Byung-Hun Kim <byunghun3@gmail.com>
2022-01-06 16:57:09 -0500

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        // "airbnb"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "semi": [1, "never"],
+        "quotes": [1, "double", { "avoidEscape": true }],
+        "indent": "off",
+        "quote-props": "off",
+        "comma-dangle": "off",
+        "import/prefer-default-export": "off"
+    }
+}
