10c3d3b9ee0f08e15bf6724b40fc7d46f525bc8b
Byung-Hun Kim <byunghun3@gmail.com>
2022-02-02 17:09:34 -0500

    create hard-coded CustomerReviewData

---
+++ src/CustomerReviewData.js
@@ -0,0 +1,18 @@
+export const CustomerReviewData = [{
+    bookId: 1,
+    user: {
+        firstName: "Anne",
+        lastName: "Carson",
+        email: "anniecars@gmail.com",
+        password: "anneCarson"
+    },
+    review: {
+        id: "1a",
+        title: "Greenlights",
+        author: "Matthew McConaughey",
+        reviewComments: "I found it interesting",
+        customerRating: 4,
+        date: "12/27/2021"
+    }
+}
+]
