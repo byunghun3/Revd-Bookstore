2c6cf19f3def8f8065f59818b05de8621ec5d00e
Byung-Hun Kim <byunghun3@gmail.com>
2022-02-05 16:15:58 -0500

    add useEffect in HeaderTwo to persist isLoggedIn state in pages with HeaderTwo component

--- src/components/HeaderTwo/HeaderTwo.tsx
+++ src/components/HeaderTwo/HeaderTwo.tsx
@@ -1,7 +1,8 @@
-import React from "react"
+import React, { useContext, useEffect } from "react"
 import { Link } from "react-router-dom"
 import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
+import { LoginContext } from "../../contexts/LoginContext"
 import { styled } from "@mui/system"
 import classes from "./HeaderTwo.module.css"
 
@@ -16,6 +17,14 @@ interface HeaderTwoProps {
 }
 
 export const HeaderTwo = (props: HeaderTwoProps) => {
+    const { setIsLoggedIn } = useContext(LoginContext)
+    const currentUser = JSON.parse(localStorage.getItem("currentUser") || "[]")
+
+    useEffect(() => {
+        if (currentUser.length) {
+            setIsLoggedIn(true)
+        } else { setIsLoggedIn(false) }
+    }, [currentUser])
     return (
         <div>
             <AppBar
