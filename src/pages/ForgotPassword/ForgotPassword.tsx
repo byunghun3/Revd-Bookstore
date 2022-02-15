1ae9e168aa3e6da2c8b59fdf271f4a4477a4478f
Byung-Hun Kim <byunghun3@gmail.com>
2022-02-15 13:29:38 -0500

    change px to rem in Login and ForgotPassword pages

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -4,61 +4,60 @@ import Grid from "@mui/material/Grid"
 import InputLabel from "@mui/material/InputLabel"
 import FormControl from "@mui/material/FormControl"
 import OutlinedInput from "@mui/material/OutlinedInput"
-import InputAdornment from "@mui/material/InputAdornment"
-import IconButton from "@mui/material/IconButton"
-import Checkbox from "@mui/material/Checkbox"
 import Button from "@mui/material/Button"
 import AccountCircleIcon from "@mui/icons-material/AccountCircle"
-import Visibility from "@mui/icons-material/Visibility"
-import VisibilityOff from "@mui/icons-material/VisibilityOff"
 import { styled } from "@mui/system"
 import classes from "./ForgotPassword.module.css"
 import { Card } from "@mui/material"
 
-const FormGrid = styled(Grid)({
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    display: "flex",
-    flexDirection: "column"
+const StyledAccountCircleIcon = styled(AccountCircleIcon)({
+    flex: "1",
+    marginTop: "5%",
+    fontSize: "3rem"
 })
 
-const LoginCard = styled(Card)({
-    justifyContent: "center",
-    alignItems: "center",
+const ForgotPWCard = styled(Card)({
     display: "flex",
     flexDirection: "column",
-    height: "70vh",
-    width: "30vw",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "5% 0 10% 0",
+    minHeight: "55vh",
+    width: "40rem"
 
 })
 
 const UsernameForm = styled(FormControl)({
-    margin: "20px",
-    width: "80%",
+    margin: "2% 0",
+    width: "60%",
+    // border: "solid black"
 
 })
 
-const PasswordForm = styled(FormControl)({
-    margin: "20px",
-    width: "80%",
-
+const StyledInputLabel = styled(InputLabel)({
+    fontSize: "1.5rem"
 })
 
-const FormInputLabel = styled(InputLabel)({
-    margin: "0"
+const StyledOutlinedInput = styled(OutlinedInput)({
+    fontSize: "1.5rem"
 })
 
-const ForgotPWButton = styled(Button)({
-    textDecoration: "none"
+const BackToLoginButton = styled(Button)({
+    margin: "2% 0",
+    // width: "41%",
+    textDecoration: "none",
+    fontSize: "1.3rem"
 })
 
-const LogInButton = styled(Button)({
-    width: "80%"
+const SendLinkButton = styled(Button)({
+    margin: "3% 0 5% 0",
+    width: "60%",
+    fontSize: "1.3rem"
 })
 
 const SignUpButton = styled(Button)({
-    textDecoration: "none"
+    textDecoration: "none",
+    fontSize: "1.3rem"
 })
 
 interface LoginProps {
@@ -69,10 +68,9 @@ export const ForgotPassword: FC<LoginProps> = ({ }) => {
     const navigate = useNavigate()
 
     const [email, setEmail] = useState("")
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
     const [showForgotPassword, setShowForgotPassword] = useState(true)
     const [resetLinkSent, setResetLinkSent] = useState(false)
-    const [emailError, setEmailError] = useState(true)
+    const [emailError, setEmailError] = useState(false)
     const users = JSON.parse(localStorage.getItem("users") || "[]")
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -83,73 +81,84 @@ export const ForgotPassword: FC<LoginProps> = ({ }) => {
         })
 
         if (emailExists) {
-            alert(`Reset link has been sent to ${email}!`)
+            // alert(`Reset link has been sent to ${email}!`)
             setShowForgotPassword(false)
             setEmailError(false)
             setResetLinkSent(true)
-            // setIsLoggedIn(true)
-            // navigate(-1)
         } else {
-            setShowForgotPassword(false)
+            setShowForgotPassword(true)
             setEmailError(true)
         }
     }
 
     return (
         <div className={classes.forgotPasswordPage}>
-            <FormGrid>
-                {showForgotPassword &&
-                    <LoginCard>
-                        <AccountCircleIcon />
-                        <div>Forgot your password?</div>
-                        <div>Please enter your email address</div>
-                        <form onSubmit={handleSubmit}>
+            {/* {showForgotPassword && */}
+            <ForgotPWCard>
+                <StyledAccountCircleIcon />
+                <form className={classes.forgotPasswordContent} onSubmit={handleSubmit}>
+                    {showForgotPassword &&
+                        <div>
+                            <div className={classes.forgotPW}>Forgot your password?</div>
+                            <div className={classes.enterEmail}>Please enter your email address</div>
                             <UsernameForm variant="outlined">
-                                <FormInputLabel>Email</FormInputLabel>
-                                <OutlinedInput
+                                <StyledInputLabel>Email</StyledInputLabel>
+                                <StyledOutlinedInput
                                     label="Email"
                                     name="Email"
                                     value={email}
                                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setEmail(e.target.value) }}
                                     required />
-                                {emailError && <div>The email you entered does not exist</div>}
+                                {emailError && <div className={classes.noEmailError}>The email you entered does not exist</div>}
                             </UsernameForm>
-                            <LogInButton type="submit" color="primary" variant="contained">
+                            <SendLinkButton type="submit" color="primary" variant="contained">
                                 Send reset link
-                            </LogInButton>
+                            </SendLinkButton>
                             <div>
                                 <Link className={classes.link} to="/login">
-                                    <Button type="button">
-                                        Back to login
-                                    </Button>
+                                    <BackToLoginButton type="button">
+                                        Back to Login
+                                    </BackToLoginButton>
                                 </Link>
                             </div>
-                            <div>
-                                <span>Don&apos;t have account?</span>
+                            <div className={classes.signUpContent}>
+                                <span className={classes.signUpText}>Don&apos;t have account?</span>
                                 <Link className={classes.link} to="/signup">
-                                    <Button type="button">
+                                    <SignUpButton type="button">
                                         Sign up
-                                    </Button>
+                                    </SignUpButton>
                                 </Link>
                             </div>
-                        </form>
-                    </LoginCard>
-                }
-                {resetLinkSent &&
-                    <LoginCard>
-                        <AccountCircleIcon />
-                        <div>Password reset link has been sent to {email}!</div>
-                        <div>
-                            <Link className={classes.link} to="/login">
-                                <Button type="button">
-                                    Back to login
-                                </Button>
-                            </Link>
                         </div>
-                    </LoginCard>
-                }
-            </FormGrid>
-            {/* <div>ehllo</div> */}
+                    }
+                    {resetLinkSent &&
+                        <div className={classes.linkSentContent}>
+                            <div className={classes.linkSent}>Password reset link has been sent to {email}!</div>
+                            <div>
+                                <Link className={classes.link} to="/login">
+                                    <BackToLoginButton type="button">
+                                        Back to login
+                                    </BackToLoginButton>
+                                </Link>
+                            </div>
+                        </div>
+                    }
+                </form>
+            </ForgotPWCard>
+            {/* } */}
+            {/* {resetLinkSent &&
+                <ForgotPWCard>
+                    <StyledAccountCircleIcon />
+                    <div className={classes.linkSent}>Password reset link has been sent to {email}!</div>
+                    <div>
+                        <Link className={classes.link} to="/login">
+                            <BackToLoginButton type="button">
+                                Back to login
+                            </BackToLoginButton>
+                        </Link>
+                    </div>
+                </ForgotPWCard>
+            } */}
         </div >
     )
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -17,14 +17,6 @@ import { styled } from "@mui/system"
 import classes from "./Login.module.css"
 import { Card } from "@mui/material"
 
-const FormGrid = styled(Grid)({
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    minHeight: "100%"
-})
-
 const LoginCard = styled(Card)({
     display: "flex",
     flexDirection: "column",
@@ -38,17 +30,19 @@ const LoginCard = styled(Card)({
 })
 
 const StyledAccountCircleIcon = styled(AccountCircleIcon)({
-    marginTop: "5%",
+    flex: "1",
+    margin: "5% 0",
     fontSize: "3rem"
 })
 
 const UsernameForm = styled(FormControl)({
-    margin: "2% 0",
+    margin: "1% 0 3% 0",
     width: "60%"
 })
 
 const PasswordForm = styled(FormControl)({
-    marginTop: "2%",
+    margin: "3% 0",
+    // marginTop: "2%",
     width: "60%"
 })
 
@@ -61,12 +55,13 @@ const StyledOutlinedInput = styled(OutlinedInput)({
 })
 
 const ForgotPWButton = styled(Button)({
+    margin: "2% 0",
     textDecoration: "none",
     fontSize: "1.3rem"
 })
 
 const LogInButton = styled(Button)({
-    margin: "1% 0",
+    margin: "2% 0",
     width: "60%",
     fontSize: "1.3rem"
 })
@@ -85,13 +80,10 @@ export const Login: FC<LoginProps> = ({ }) => {
     const navigate = useNavigate()
     const hardCodedUsers = UsersData
     const [email, setEmail] = useState("")
-    const [emailMatchError, setEmailMatchError] = useState("")
-    const [isEmailInvalid, setIsEmailInvalid] = useState(false)
+    const [isUserInvalid, setUserInvalid] = useState(false)
     const [password, setPassword] = useState("")
     const [showPassword, setShowPassword] = useState(false)
-    const [passwordError, setPasswordError] = useState("")
-    const [isPasswordInvalid, setIsPasswordInvalid] = useState(false)
-    const { isLoggedIn, setIsLoggedIn } = useContext(LoginContext)
+    const { setIsLoggedIn } = useContext(LoginContext)
     const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("currentUser") || "[]"))
     const users = JSON.parse(localStorage.getItem("users") || JSON.stringify(hardCodedUsers))
 
@@ -112,9 +104,7 @@ export const Login: FC<LoginProps> = ({ }) => {
 
     console.log(email)
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-
+    const handleLogIn = (e: React.FormEvent<HTMLFormElement>) => {
         const userExists = users.filter((el: any) => {
             return email === el.email
         }).find((el: any) => {
@@ -122,9 +112,6 @@ export const Login: FC<LoginProps> = ({ }) => {
         })
 
         if (userExists) {
-            setEmailMatchError("")
-            setIsEmailInvalid(false)
-
             let newCurrentUser = [...currentUser]
 
             const currentUserFirstName = users.filter((el: any) => {
@@ -150,85 +137,78 @@ export const Login: FC<LoginProps> = ({ }) => {
 
             localStorage.setItem("currentUser", JSON.stringify(newCurrentUser))
 
+            navigate(-1)
+
             setIsLoggedIn(true)
-
-            // localStorage.setItem("isLoggedIn", isLoggedIn)
-
-            // navigate(-1)
         } else {
-            setEmailMatchError("Incorrect email or password")
-            setIsEmailInvalid(true)
+            e.preventDefault()
+            setUserInvalid(true)
         }
     }
 
     return (
         <div className={classes.loginPage}>
-            {/* <FormGrid> */}
-            <Grid item sm={12} md={6} lg={4}>
-                <LoginCard>
-                    <StyledAccountCircleIcon />
-                    <form onSubmit={handleSubmit}>
-                        <div className={emailMatchError !== "" ? classes.passwordErrorMessage : classes.passwordNoErrorMessage}>{emailMatchError}</div>
-                        <UsernameForm variant="outlined">
-                            <StyledInputLabel>Email</StyledInputLabel>
-                            <StyledOutlinedInput
-                                label="Email"
-                                name="Email"
-                                value={email}
-                                // error={isEmailInvalid}
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setEmail(e.target.value) }}
-                                required />
-                        </UsernameForm>
-                        <PasswordForm>
-                            <StyledInputLabel>Password</StyledInputLabel>
-                            <StyledOutlinedInput
-                                label="Password"
-                                name="Password"
-                                type={showPassword ? "text" : "password"}
-                                value={password}
-                                // error={isPasswordInvalid}
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setPassword(e.target.value) }}
-                                endAdornment={<InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        // size="large"
-                                        onClick={handleClickShowPassword}
-                                        // onMouseDown={handleMouseDownPassword}
-                                        edge="end"
-                                    >
-                                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                                    </IconButton>
-                                </InputAdornment>}
-                                required />
-                            <div className={passwordError !== "" ? classes.passwordErrorMessage : classes.passwordNoErrorMessage}>{passwordError}</div>
-                        </PasswordForm>
-                        <div>
-                            {/* <span>
+            <LoginCard>
+                <StyledAccountCircleIcon />
+                <form className={classes.loginContent} onSubmit={handleLogIn}>
+                    {isUserInvalid && <div className={classes.noUserExists}>Incorrect email or password</div>}
+                    {/* <div className={userMatchError !== "" ? classes.passwordErrorMessage : classes.passwordNoErrorMessage}>{userMatchError}</div> */}
+                    <UsernameForm variant="outlined">
+                        <StyledInputLabel>Email</StyledInputLabel>
+                        <StyledOutlinedInput
+                            label="Email"
+                            name="Email"
+                            value={email}
+                            // error={isEmailInvalid}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setEmail(e.target.value) }}
+                            required />
+                    </UsernameForm>
+                    <PasswordForm>
+                        <StyledInputLabel>Password</StyledInputLabel>
+                        <StyledOutlinedInput
+                            label="Password"
+                            name="Password"
+                            type={showPassword ? "text" : "password"}
+                            value={password}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setPassword(e.target.value) }}
+                            endAdornment={<InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    // size="large"
+                                    onClick={handleClickShowPassword}
+                                    // onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>}
+                            required />
+                        {/* <div className={passwordError !== "" ? classes.passwordErrorMessage : classes.passwordNoErrorMessage}>{passwordError}</div> */}
+                    </PasswordForm>
+                    {/* <span>
                             <Checkbox />
                             <span>Remember me</span>
                         </span> */}
-                            <Link className={classes.link} to="/forgotpassword">
-                                <ForgotPWButton type="button">
-                                    Forgot password?
-                                </ForgotPWButton>
-                            </Link>
-                        </div>
-                        <LogInButton color="primary" variant="contained" type="submit">
-                            Log in
-                        </LogInButton>
-                        <div className={classes.signUpContent}>
-                            <span className={classes.signUpText}>Don&apos;t have account?</span>
-                            <Link className={classes.link} to="/signup">
-                                <SignUpButton type="button">
-                                    Sign up
-                                </SignUpButton>
-                            </Link>
-                        </div>
-                    </form>
-                </LoginCard>
-            </Grid>
-            {/* </FormGrid> */}
-            {/* <div>ehllo</div> */}
+                    <div>
+                        <Link className={classes.link} to="/forgotpassword">
+                            <ForgotPWButton type="button">
+                                Forgot password?
+                            </ForgotPWButton>
+                        </Link>
+                    </div>
+                    <LogInButton color="primary" variant="contained" type="submit">
+                        Log in
+                    </LogInButton>
+                    <div className={classes.signUpContent}>
+                        <span className={classes.signUpText}>Don&apos;t have account?</span>
+                        <Link className={classes.link} to="/signup">
+                            <SignUpButton type="button">
+                                Sign up
+                            </SignUpButton>
+                        </Link>
+                    </div>
+                </form>
+            </LoginCard>
         </div >
     )
 }
